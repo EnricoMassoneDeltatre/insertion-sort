78d69bbb0ec79afda7079f27cfc63ef0ab85c990
enrico.massone@deltatre.it <enrico.massone@deltatre.it>
2019-10-23 00:14:08 +0200

    wip

--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 const { insertionSort } = require("./index");
-const { assert } = require("chai");
+const { assert, expect } = require("chai");
 
 test("passing argument of type number throws a TypeError", () => {
     try {
@@ -10,4 +10,108 @@ test("passing argument of type number throws a TypeError", () => {
     }
     
     throw new Error("Should not reaching here");
+});
+
+test("passing argument of type string throws a TypeError", () => {
+    try {
+        const _ = insertionSort("test");
+    } catch (error) {
+        assert.instanceOf(error, TypeError);
+        return;
+    }
+    
+    throw new Error("Should not reaching here");
+});
+
+test("passing argument of type boolean throws a TypeError", () => {
+    try {
+        const _ = insertionSort(true);
+    } catch (error) {
+        assert.instanceOf(error, TypeError);
+        return;
+    }
+    
+    throw new Error("Should not reaching here");
+});
+
+test("passing argument of type null throws a TypeError", () => {
+    try {
+        const _ = insertionSort(null);
+    } catch (error) {
+        assert.instanceOf(error, TypeError);
+        return;
+    }
+    
+    throw new Error("Should not reaching here");
+});
+
+test("passing argument of type undefined throws a TypeError", () => {
+    try {
+        const _ = insertionSort(undefined);
+    } catch (error) {
+        assert.instanceOf(error, TypeError);
+        return;
+    }
+    
+    throw new Error("Should not reaching here");
+});
+
+test("passing argument of type object throws a TypeError", () => {
+    try {
+        const _ = insertionSort({key: "value"});
+    } catch (error) {
+        assert.instanceOf(error, TypeError);
+        return;
+    }
+    
+    throw new Error("Should not reaching here");
+});
+
+test("passing argument of type symbol throws a TypeError", () => {
+    try {
+        const _ = insertionSort(Symbol());
+    } catch (error) {
+        assert.instanceOf(error, TypeError);
+        return;
+    }
+    
+    throw new Error("Should not reaching here");
+});
+
+test("passing argument of type function throws a TypeError", () => {
+    try {
+        const _ = insertionSort(() => [1, 2, 3]);
+    } catch (error) {
+        assert.instanceOf(error, TypeError);
+        return;
+    }
+    
+    throw new Error("Should not reaching here");
+});
+
+test("an empty array is returned unchanged", () => {
+    // ACT
+    const result = insertionSort([]);
+
+    // ASSERT
+    assert.isArray(result);
+    assert.isEmpty(result);
+});
+
+test("a single item array is returned unchanged", () => {
+    // ACT
+    const result = insertionSort([23]);
+
+    // ASSERT
+    assert.isArray(result);
+    expect(result).to.eql([23]);
+});
+
+test("an ordered array is returned unchanged", () => {
+    // ACT
+    const result = insertionSort([1, 2, 3]);
+
+    // ASSERT
+    assert.isArray(result);
+    expect(result).to.eql([1, 2, 3]);
 });
